af8e8ba2f040dbfd69ed1a4b397369bad17092b7
sweetArizCider <arizah.applecider+github@gmail.com>
2025-03-11 18:53:56 -0600

    Modified

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.post('/api/shorturl', (req, res) =>{
+  
+  res.json({ original_url : 'https://freeCodeCamp.org', short_url : 1});
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
